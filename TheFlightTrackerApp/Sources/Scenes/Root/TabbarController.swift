2c2b9751d0a84d2d32bfb0b2e7f4ced8bf5c71a3
Ilhansari <ilhansari94@hotmail.com>
2021-11-07 13:41:02 +0300

    Create `TabbarController` and add icons for tab bar items

---
+++ TheFlightTrackerApp/Sources/Scenes/Root/TabbarController.swift
@@ -0,0 +1,98 @@
+//
+//  TabbarController.swift
+//  TheFlightTrackerApp
+//
+//  Created by Ilhan Sari on 7.11.2021.
+//
+
+import UIKit
+
+private enum TabBarItemType: Int {
+    case airports = 0, flights = 1, settings = 2
+}
+
+final class TabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureTabbarItems()
+    }
+
+    override func setViewControllers(_ viewControllers: [UIViewController]?, animated: Bool) {
+        super.setViewControllers(viewControllers, animated: animated)
+        configureTabbar()
+    }
+}
+
+// MARK: - Configure Tabbar
+private extension TabbarController {
+
+    private func configureTabbarItems() {
+        let controllers = configureTabbarController()
+        setViewControllers(controllers, animated: false)
+    }
+
+    func configureTabbar() {
+        tabBar.isTranslucent = false
+        view.backgroundColor = .black
+        UITabBar.appearance().tintColor = .gray
+    }
+}
+
+// MARK: - Helpers
+private extension TabbarController {
+    private func configureTabbarController() -> [UIViewController] {
+
+        let airportsNavigationViewController = createTabbarItem(for: AirportsViewController(),
+                                                                title: "Airports",
+                                                                image: .airportsIcon,
+                                                                tag: .airports)
+
+        let flightsNavigationViewController = createTabbarItem(for: FlightsViewController(),
+                                                               title: "Flights",
+                                                               image: .flightsIcon,
+                                                               tag: .flights)
+
+        let settingsNavigationViewController = createTabbarItem(for: SettingsViewController(),
+                                                                title: "Settings",
+                                                                image: .settingsIcon,
+                                                                tag: .settings)
+
+        return [airportsNavigationViewController,
+                flightsNavigationViewController,
+                settingsNavigationViewController]
+    }
+
+    private func createTabbarItem(for viewController: UIViewController,
+                                  title: String,
+                                  image: UIImage?,
+                                  tag: TabBarItemType) -> UIViewController {
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.tintColor = .blue
+        navigationController.tabBarItem = UITabBarItem(title: title, image: image, tag: tag.rawValue)
+        return navigationController
+    }
+}
+
+
+class AirportsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
+class FlightsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
+class SettingsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
